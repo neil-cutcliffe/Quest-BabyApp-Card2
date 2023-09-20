3c73bf0e9835340452e8b72de90d4b9ce2905859
Neil Cutcliffe <neil@Neils-Desktop.lan>
2023-09-20 14:44:27 -0700

    Added definition and static values for props.post.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,13 @@ function App() {
       <StyledEngineProvider injectFirst>
         <ThemeProvider theme={questTheme}>
           <Routes>
-            <Route path="/" element={<SampleCard />} />
+            <Route path="/" element={<SampleCard post={
+              {
+                 title   : 'Title Neil 3',
+                 content : 'Content Neil was here three.',
+                 image   : 'url(https://babyapps.io/test-1/wp-content/uploads/sites/14/2023/01/8344BA1F-CBA1-4E95-846C-66C913A43E30-2-scaled.jpeg)'
+              }
+            } />} />
           </Routes>
         </ThemeProvider>
       </StyledEngineProvider>
